3bc19e1854082e94c7dac2582655fec6d0f63dc1
Andrew Marek <andwmarek@gmail.com>
2023-12-05 15:45:45 -0800

    update day 4 fin

--- day-four/sol.py
+++ day-four/sol.py
@@ -1,41 +1,51 @@
-file_name = "input.txt"
+from typing import DefaultDict
+
+file_name = "test.txt"
 with open(file_name, 'r') as f:
     data = f.read()
 lines = data.split("\n")
 
 def part_one():
-    total_points = 0
-
-    for line in lines:
-        matches = []
-        card_points = 0
-
-        card_part, rest = line.split(":")
-
-        card_no = card_part.split(" ")[-1]
-        winners, mine = rest.strip().split("|")
-
-        winning_set = set([winner.strip() for winner in winners.strip().split(" ") if winner != ''])
-        print(winning_set)
-        for number in mine.strip().split(" "):
-            if number in winning_set:
-                if len(matches) >= 1:
-                    card_points *= 2
-                else:
-                    card_points += 1
-                matches.append(number)
-
-        print(card_points)
-        total_points += card_points
-            
-
-        # print(f"|{winners}|")
+    p1 = 0
+    for i, line in enumerate(lines):
+        card, rest = line.split(":")
+        _id = int(card.split(" ")[-1])
         
-    return total_points
+        winners, mine = rest.split("|")
+
+        winning_nums = [int(x) for x in winners.split()]
+        my_nums = [int(x) for x in mine.split()]
+        val = len(set(winning_nums) & set(my_nums))
+        if val > 0: p1 += 2**(val-1)
+    return p1
 
 
 def part_two():
-    pass
+    card_counts = DefaultDict(int)
+    for i, line in enumerate(lines):
+        card_counts[i] += 1
+        _, rest = line.split(":")
+        
+        winners_str, my_numbers_str = rest.split("|")
+
+        winning_nums = [int(x) for x in winners_str.split()]
+        my_nums = [int(x) for x in my_numbers_str.split()]
+
+        matches = len(set(winning_nums) & set(my_nums))
+        print(f"*** matches: {matches}")
+        for j in range(matches):
+            print(card_counts)
+            card_counts[i+1+j] += card_counts[i]
+
+    total_cards = sum(card_counts.values())
+    return total_cards
+
+
+
+
+
+
+
 
 print(f"Part one: {part_one()}")
 print(f"Part two: {part_two()}")
--- day-three/sol.py
+++ day-three/sol.py
@@ -3,7 +3,8 @@ from typing import DefaultDict
 
 symbols = set() 
 
-with open("test.txt", "r") as f:
+input_file_name = "input.txt"
+with open(input_file_name, "r") as f:
     data = f.read()
     lines = data.split("\n")
     for l in lines:
@@ -11,8 +12,8 @@ with open("test.txt", "r") as f:
             if not x.isdigit() and x != '.':
                 symbols.add(x)
 
-def part_one():
-    with open("input.txt", "r") as f:
+def part_one(symbols):
+    with open(input_file_name, "r") as f:
         data = f.read()
         lines = data.split("\n")
 
@@ -26,21 +27,6 @@ def part_one():
                     symbol_coords[i].append(j)
 
         # get the coordinates of the number
-        '''
-        # key = index of number
-        number_coords = {
-            0: [{
-                "value": "1234",
-                "span": (0, 4),
-                "is_part_no": False
-            },
-            {
-                "value": "1234",
-                "span": (0, 4),
-                "is_part_no": False
-            },
-        ]}
-        '''
         number_coords = DefaultDict(list)
         for i in range(len(lines)):
             line = lines[i]
@@ -58,55 +44,22 @@ def part_one():
 
                 below_row = row + 1
                 above_row = row - 1
+                row_types = {below_row, above_row, row}
+                for row_type in row_types: 
+                    if row_type in symbol_coords.keys():
+                        indices = symbol_coords[below_row]
 
-                if below_row in symbol_coords.keys():
-                    indices = symbol_coords[below_row]
+                        for index in indices:
+                            if index in range(cur_span[0]-1, cur_span[-1]+1):
+                                num_obj["is_part_no"] = True
 
-                    for index in indices:
-                        if index in range(cur_span[0]-1, cur_span[-1]+1):
-                            num_obj["is_part_no"] = True
-
-                if above_row in symbol_coords.keys():
-                    indices = symbol_coords[above_row]
-                    for index in indices:
-                        if index in range(cur_span[0]-1, cur_span[-1]+1):
-                            num_obj["is_part_no"] = True
-
-                if row in symbol_coords.keys():
-                    indices = symbol_coords[row]
-                    for index in indices:
-                        if index in range(cur_span[0]-1, cur_span[-1]+1):
-                            num_obj["is_part_no"] = True
-                
         sum_of_parts = 0                    
         for index, num_objs in number_coords.items():
             for num_obj in num_objs:
                 if num_obj["is_part_no"]:
                     sum_of_parts += int(num_obj["value"])
-        '''                
-        for current_row, number_coords in number_coords.items():
-            above_row = current_row - 1
-            below_row = current_row + 1
+    return sum_of_parts 
 
-            if current_row in symbol_coords.keys():
-                
-                print(current_row)
-                for symbol_coord in symbol_coords[current_row]:
-                    for number_coord in number_coords:
-                        print(number_coord, symbol_coord)
-                        if number_coord[1] == symbol_coord or number_coord[0] - 1 == symbol_coord:
-        '''                            
-
-
-        # pretty print the symbol and number coords
-        for index, coords in number_coords.items():
-            print(index, coords)
-        print("*********")
-        for index, symbol_coord in symbol_coords.items():
-            print(index, symbol_coord)
-        # print(symbol_coords) 
-        print(sum_of_parts)
-    return ""
 
 def part_two(symbols):
     with open("input.txt", "r") as f:
@@ -137,10 +90,10 @@ def part_two(symbols):
         # parse using the two sets of coords 
         for row, num_objs in number_coords.items():
             for num_obj in num_objs:
-                cur_span = num_obj["span"]                
 
                 below_row = row + 1
                 above_row = row - 1
+                cur_span = num_obj["span"]                
 
                 for row_type in {below_row, above_row, row}:
                     symbols = symbol_objs[row_type]
@@ -158,36 +111,29 @@ def part_two(symbols):
                 if num_obj["is_part_no"]:
                     sum_of_parts += int(num_obj["value"])
 
-
-        # pretty print the symbol and number coords
-        for index, coords in number_coords.items():
-            print(index, coords)
-        print("*********")
-        for index, symbol_coord in symbol_objs.items():
-            print(index, symbol_coord)
-        # print(symbol_coords) 
-        print(sum_of_parts)
-
-
         product_ratio_sum = 0
         for index, symbol_objs in symbol_objs.items():
             for symbol_obj in symbol_objs:
                 cur_product_ratio = 1
                 if len(symbol_obj["adj_list"]) == 2:
-                    print(symbol_obj["adj_list"])
                     for neighbor in symbol_obj["adj_list"]:
                         cur_product_ratio *= neighbor 
                 else: continue
                 product_ratio_sum += cur_product_ratio
-    print(product_ratio_sum)
-    return ""
+    return product_ratio_sum
 
 
-#part_one()
-part_two(symbols)
+symbols = set() 
 
-'''
+input_file_name = "input.txt"
+with open(input_file_name, "r") as f:
+    data = f.read()
+    lines = data.split("\n")
+    for l in lines:
+        for x in l:
+            if not x.isdigit() and x != '.':
+                symbols.add(x)
 
-'''
-                
-# 508875 bad 
+print(f"Part 1: {part_one(symbols)}")
+print(f"Part 2: {part_two(symbols)}")
+
